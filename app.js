b77d64a70d14a00a1658d42e40ee3b7cc5386dbf
RameenKhannnn <rameenkhan8787@gmail.com>
2024-07-18 03:26:54 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,135 @@
+// function fullName(){
+//     var fName = "huda";
+//     var lName = "ayaz";
+//     console.log(fName + " " + lName);
+// }
+
+
+// fullName()
+
+
+
+
+                // paramter
+// function fullName(fName,lName){
+   
+//     console.log(fName + " " + lName);
+
+// }
+
+// var firstName = prompt("enter your first name")
+// var lastName = prompt("enter your last name")
+
+// fullName(firstName,lastName)  //aurgument
+
+
+// var arr = []
+// function myUnshift (a){
+//    arr[0] = a
+// }
+
+// myUnshift("apple")
+// console.log(arr);
+
+
+// type
+
+// function myFunc (a,b,c){
+//     console.log(a,b);
+// }
+
+// myFunc(1,"cow","true")
+
+var count = 0
+ function counter(){
+   
+    for(var i = 0; i < 4;i++){
+        count++
+        console.log(count,i);
+        if(count === 7){
+            console.log(i); //2
+            break;
+        }
+    }
+ }
+ // function yourFunc (one,two){
+//     var fruitName = one + " " + two
+//     console.log(fruitName);
+// }
+
+// yourFunc("melon")  // mwlon undefined
+
+
+
+// function sub (one,two ){
+//     var subtract = one +two
+//     console.log(subtract);
+// }
+
+
+// var userInp = +prompt("enter your number")
+// var userInp2 = +prompt("enter your second number ")
+// sub(userInp,userInp2)
+// var user = prompt()
+// console.log(user);
+// ======================== Return =============
+
+//The return statement stops the execution 
+// of a function and returns a value.
+
+// function myFunction (a ,b){
+//    return a + b 
+   
+  
+// }
+
+// // console.log(myFunction(3,4))   
+// var reult = myFunction(3,5) 
+// console.log(reult);
+
+
+// function myFunction (a ,b){
+//     var sum =  a + b
+    
+//     return sum
+    
+   
+//  }
+ 
+ 
+// console.log(myFunction(4,5)+2)
+
+
+// function output (a,b){
+//     var sum = a + b
+//     return sum
+// } 
+// console.log(output(2,3));
+
+
+// function fruitChecking(fruit){
+//    var fruitList = ["apple","mango","bnana","orange"]
+//    if(fruitList.indexOf(fruit) !== -1){
+//     return "lai hon"
+//    }else{
+//     return " nhi lai hon"
+//    }
+// }
+
+// console.log(fruitChecking("grapes"));
+// var sum = 3 + 2
+
+// function add (){
+//     var sum = 9
+//     console.log(sum);
+// }
+// console.log(sum);
+
+
+var theSum
+ addNumbers();
+  function addNumbers() {
+ theSum = 2 + 2;
+  }
+  
+
